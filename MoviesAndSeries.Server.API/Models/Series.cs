9e76a78b4346c548b88fbd5fe0501a465ee941ca
Ni9er120404 <Niger120404@yandex.ru>
2023-06-17 22:15:27 +0300

    The "Series" model has been created

---
+++ MoviesAndSeries.Server.API/Models/Series.cs
@@ -0,0 +1,30 @@
+﻿namespace MoviesAndSeries.Server.API.Models
+{
+	public class Series : Information
+	{
+		public ICollection<Episode>? Episodes { get; set; } = new List<Episode>();
+
+		public uint TotalDuration
+		{
+			get
+			{
+				uint totalDuration = 0;
+
+				foreach (Episode? episode in Episodes!.Where(episode => episode is not null))
+				{
+					totalDuration += episode.Duration;
+				}
+
+				return totalDuration;
+			}
+		}
+
+		public ushort? StartDate { get; set; }
+
+		public ushort? EndDate { get; set; }
+
+		public User User { get; set; } = new();
+
+		public Guid UserId { get; set; }
+	}
+}
