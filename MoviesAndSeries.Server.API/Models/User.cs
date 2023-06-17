8b8346164a67ce4f59826c2eb3e573bec01f361e
Ni9er120404 <Niger120404@yandex.ru>
2023-06-17 22:15:36 +0300

    The "User" model has been created

---
+++ MoviesAndSeries.Server.API/Models/User.cs
@@ -0,0 +1,17 @@
+﻿namespace MoviesAndSeries.Server.API.Models
+{
+	public class User
+	{
+		public Guid Id { get; set; }
+
+		public string UserName { get; set; } = null!;
+
+		public string Email { get; set; } = null!;
+
+		public string Password { get; set; } = null!;
+
+		public ICollection<Series> Series { get; set; } = new List<Series>();
+
+		public ICollection<Movie>? Movies { get; set; } = new List<Movie>();
+	}
+}
