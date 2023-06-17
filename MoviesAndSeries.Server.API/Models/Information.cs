7b584d4a6c055c2e67a07f8f4f01dad96d0a7737
Ni9er120404 <Niger120404@yandex.ru>
2023-06-17 22:15:06 +0300

    The "Information" model has been created

---
+++ MoviesAndSeries.Server.API/Models/Information.cs
@@ -0,0 +1,17 @@
+﻿namespace MoviesAndSeries.Server.API.Models
+{
+	public class Information
+	{
+		public int Id { get; set; }
+
+		public string Name { get; set; } = null!;
+
+		public string? Description { get; set; }
+
+		public string? Image { get; set; }
+
+		public ushort? Rating { get; set; }
+
+		public uint Duration { get; set; }
+	}
+}
