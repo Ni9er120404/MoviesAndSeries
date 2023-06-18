cda4869666c7407e9ebe90eff7228857a8a6eac8
Ni9er120404 <Niger120404@yandex.ru>
2023-06-19 01:29:09 +0300

    A UserInformation model has been created for registering a new user

---
+++ MoviesAndSeries.Server.API/Models/UserInformation.cs
@@ -0,0 +1,13 @@
+﻿namespace MoviesAndSeries.Server.API.Models
+{
+	public class UserInformation
+	{
+		public Guid Id { get; set; }
+
+		public string UserName { get; set; } = null!;
+
+		public string Email { get; set; } = null!;
+
+		public string Password { get; set; } = null!;
+	}
+}
