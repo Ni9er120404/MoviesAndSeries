deecdf50ece2d2c226b0f22f02d1fe98206be351
Ni9er120404 <Niger120404@yandex.ru>
2023-06-19 16:20:41 +0300

    Fixed the logic and operation of the parser

--- MoviesAndSeries.Server.API/Parser/SeriesParser.cs
+++ MoviesAndSeries.Server.API/Parser/SeriesParser.cs
@@ -16,7 +16,7 @@ namespace MoviesAndSeries.Server.API.Parser
 		{
 			List<Series> result = new();
 
-			foreach (KeyValuePair<Series, string> series in await ParsingOneSeries())
+			foreach (KeyValuePair<Series, string> series in await ParsingInformationAboutAllSeries())
 			{
 				result.Add(series.Key);
 			}
@@ -24,53 +24,50 @@ namespace MoviesAndSeries.Server.API.Parser
 			return result;
 		}
 
-		private async Task<Dictionary<Series, string>> ParsingOneSeries()
+		private static async Task<string> ParsingOneSeries(string url)
 		{
-			Dictionary<Series, string> pairs = new();
+			string html = await GetHtml(url);
+			string ratingPattern = @"<div class=""rate-element long""[^>]+>([\d.]+)</div>";
+			string imagePattern = @"<img itemprop=""image"" src=""([^""]+)"" alt=""[^""]+"">";
+			_ = Regex.Match(html, ratingPattern);
+			Match imageMatch = Regex.Match(html, imagePattern);
 
-			foreach (KeyValuePair<Series, string> item in await ParsingInformationAboutAllSeries())
+			string result = string.Empty;
+
+			if (imageMatch.Success)
 			{
-				string html = await GetHtml(item.Value);
-				string pattern = @"<img itemprop=""image"" src=""([^""]+)"" alt=""[^""]+"">";
-
-				Match match = Regex.Match(html, pattern);
-
-				if (match.Success)
-				{
-					string imageUrl = match.Groups[1].Value;
-					item.Key.Image = _url + imageUrl;
-					pairs.Add(item.Key, imageUrl);
-				}
+				result = imageMatch.Groups[1].Value;
 			}
 
-			return pairs;
+			return result;
 		}
 
 		private async Task<Dictionary<Series, string>> ParsingInformationAboutAllSeries()
 		{
 			string html = await GetHtml(_url);
-			string pattern = @"</li> <li class=""literal__item not-loaded"" data-kp=""([\d.]+)"" data-imdb=""([\d.]+)"" data-start-year=""(\d{4})"" data-end-year=""(\d{4})"" data-genres=""([^""]+)"" data-countries=""([^""]+)"" data-status=""([^""]+)"" data-id=""(\d+)"">\s*<a href=""([^""]+)"">([^<]+)</a>";
+			string pattern = @"<li class=""literal__item not-loaded"" data-kp=""([\d.]+)"" data-imdb=""([\d.]+)"" data-start-year=""(\d{4})"" data-end-year=""(\d{4})"" data-genres=""([^""]+)"" data-countries=""([^""]+)"" data-status=""([^""]+)"" data-id=""(\d+)"">\s*<a href=""([^""]+)"">([^<]+)</a>";
 
 			MatchCollection matches = Regex.Matches(html, pattern);
 
 			Dictionary<Series, string> seriesList = new();
 
-			for (int i = 0; i < matches.Count; i++)
+			foreach (Match match in matches.Cast<Match>().Take(100))
 			{
-				Match match = matches[i];
-
 				string imdb = match.Groups[2].Value;
 				string startYear = match.Groups[3].Value;
 				string endYear = match.Groups[4].Value;
-				string id = match.Groups[9].Value;
+				string id = $"{_url}{match.Groups[9].Value}";
 				string title = match.Groups[10].Value;
+				string poster = await ParsingOneSeries(id);
 
 				Series series = new()
 				{
+					PosterId = id,
+					Rating = double.TryParse(imdb, out double rating) ? rating : null,
+					StartDate = ushort.TryParse(startYear, out ushort startDate) ? startDate : null,
+					EndDate = ushort.TryParse(endYear, out ushort endDate) ? endDate : null,
 					Name = title,
-					Rating = (ushort?)(ushort.TryParse(imdb, out ushort rating) ? rating : 0),
-					StartDate = (ushort?)(ushort.TryParse(startYear, out ushort startDate) ? startDate : 0),
-					EndDate = (ushort?)(ushort.TryParse(endYear, out ushort endDate) ? endDate : 0),
+					Image = poster
 				};
 
 				seriesList.Add(series, id);
@@ -79,19 +76,18 @@ namespace MoviesAndSeries.Server.API.Parser
 			return seriesList;
 		}
 
-		private static async Task<string> GetHtml(string url)
+		public static async Task<string> GetHtml(string url)
 		{
+			using HttpClient client = new();
+
+			using HttpResponseMessage response = await client.GetAsync(url).ConfigureAwait(false);
 			string html = string.Empty;
 			try
 			{
-				using HttpClient client = new();
-
-				html = await client.GetStringAsync(url);
+				_ = response.EnsureSuccessStatusCode();
+				html = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
 			}
-			catch (Exception ex)
-			{
-				Console.WriteLine("Ошибка при получении HTML: " + ex.Message);
-			}
+			catch { }
 
 			return html;
 		}
